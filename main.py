666073a82feaf69b301f239772b7f264b4bd8094
Rachelle Hooley <nrhooley@gmail.com>
2026-03-11 14:35:39 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+
+
+# 1. Explain a relational database in your own words:
+    # It's a database that can be connected to another database, based on common information in the database (in the tables).
+
+# 2. What is a relational database table?
+    # A set of columns and rows (Also called a relation.)
+
+# 3. What is a column in a relational databse table?
+    # A column is the label, like on a calendar the colums would be the days of the week: Sun, Mon, Tue, Wed, etc.
+
+# 4. What is a row in a relational database table?
+    # A row is the record or the item. Like on a calendar it'd be what day of the month it is: 1, 2, 3, 4, 5, etc.
+
+
+
+# Create: Creating a new record in a database. This is performed with an INSERT command
+# Read: Reading one or more records from a database. We read records using the SELECT command
+# Update: Updating existing records in a database. Not surprisingly, we use the UPDATE command to update records
+# Delete: Deleting one or more records in a database. Also not surprisingly, we use the DELETE command to delete records from a database
+
+
+
+# CREATE - is used to define the database structure schema
+# DROP - removes tables and databases
+# ALTER - Modifies tables by adding or removing columns, indexes or constraints.
